3fd5324d89f3b56784de4bb6da2bdb7ef00cf4b8
Taylor Britton <tbrit10@gmail.com>
2021-10-18 15:48:28 -0700

    Re-organize work profile loading in config.el

--- config.el
+++ config.el
@@ -91,6 +91,13 @@
       +doom-dashboard-menu-sections))
 
 ;; ----------------
+;; Profiles Stuff
+;; ----------------
+
+(if (getenv "WORK")
+    (load! "work-config.el" "work-config"))
+
+;; ----------------
 ;; Org Stuff
 ;; ----------------
 (after! org (setq
@@ -115,9 +122,6 @@
         (org-agenda-files :maxlevel . 2))
   (add-hook 'org-mode-hook #'visual-line-mode)
   )
-
-(if (getenv "WORK")
-    (load! "work-config.el" "work-config"))
 
 (setq org-agenda-custom-commands
     '(
